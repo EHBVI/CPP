4f7b4ac442593bf9479b626635f78cf3bc2a4712
Abhi <jabhivarun@gmail.com>
2023-06-27 14:12:31 +0530

    the first commit on main1 branch

--- work.cpp
+++ work.cpp
@@ -4,15 +4,6 @@ using namespace std;
 int main(){
    int n;
    cin>>n;
-   int i = 0;
-   for (int i = 2; i <= n-1; i++)
-   {
-    if (n % i == 0)
-    {
-        cout<<"not a prime number";
-    }
-   }    
-    cout<<"it is a prime number";
-   
-    return 0;
+   cout<<(n*n)<<endl;
+   return 0;
 }
